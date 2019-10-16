4dfe663585ce46ef397d8a7cfd6c0befab7cfa1d
Ole Flaten <ole.flaten@inn.no>
2019-10-16 11:21:37 +0200

    Added SubRepo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void arrayTest(int(&arrayIn)[10])
+{
+    for(int i{0}; i < 10; i++ )
+        cout <<  arrayIn[i] << endl;
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    int array[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    arrayTest(array);
+    return 0;
+}
